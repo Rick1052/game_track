3c860e040c9589f1ab01c7d9d83999b6d5272ec9
Rick1052 <sant.gp.henrique@gmail.com>
2025-11-15 12:40:08 -0300

    config firebase google 15-11

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,9 +7,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.game_track"
+    namespace = "com.example.gametrack"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.game_track"
+        applicationId = "com.example.gametrack"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
